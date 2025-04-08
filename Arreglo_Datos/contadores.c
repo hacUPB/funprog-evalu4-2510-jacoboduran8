6f997a83fa6150b7e7766dcaf63ae9a1a608f500
Jacobo <jacoboduranaristizabal@gmail.com>
2025-04-08 09:43:01 -0500

    Se trabajo en la primera parte de la Unidad 4

---
+++ Arreglo_Datos/contadores.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+    int Datos[5] = {0}; // 5 Contadores
+
+    // Hay que incrementar los contadores
+    // Y que mustre los valores
+
+    for (int cont = 0; cont < 15; cont++)
+    {
+        Datos[0]++;
+        Datos[1]+=2;
+        Datos[2]+=3;
+        Datos[3]+=4;
+        Datos[4]+=5;
+    }
+
+    for (int i = 0; i<5; i++)
+    {
+        printf("%d - %p\n", Datos[i], &Datos[i]);
+    }
+  
+    return 0;
+      
+}
---
+++ Arreglo_Datos/temperatura.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+# include <stdlib.h>
+
+int main(){
+    srand(time(NULL)); // Generamos la semilla
+    int temp[24]; // Con esto tenemos ya declara las 24 variables
+    float media = 0;
+    int hora;
+
+    for(hora = 0; hora < 24; hora++)
+    {
+        rand():
+    }
+
+    return 0;
+}

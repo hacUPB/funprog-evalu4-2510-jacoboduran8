b7b51c3bfaa762df6e727a7598b31a081560c5ad
Jacobo <jacoboduranaristizabal@gmail.com>
2025-05-08 09:40:13 -0500

    Se subieron los avances hechos en clase

--- Archivos/caracteres.c
+++ Archivos/caracteres.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
-
-int main(){
+ 
+int main(int argc, char const *argv[])
+{
     FILE *pfile;
-    int letra = '1';
+    int letra;
     char caracter;
-
     pfile = fopen("Texto2.txt", "a");
-    if(pfile){ //NULL es falso, cualquier valor diferente es verdadero
-        while(letra != '?'){
-            caracter = fgetc(stdin);
+    if (!pfile) // NULL es falso, cualquier valor diferente es verdadero
+    {
+        perror("No se pudo abrir el archivo.\n");
+        return 1;
+    }
+    while (caracter != '?')
+    {
+        caracter = fgetc(stdin);
+        if (caracter != '?')
+        {
             letra = fputc(caracter, pfile);
         }
     }
+ 
     fclose(pfile);
-
-    return 0;
+    return 0; //Retorno al sistema operativo sin errores
 }

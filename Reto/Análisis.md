3988de6a223ab9810ea37634f9d026909adb0b13
Jacobo <jacoboduranaristizabal@gmail.com>
2025-05-16 23:13:26 -0500

    Se subio el Reto terminado, con su respectivo analisis

--- Reto/Análisis.md
+++ Reto/Análisis.md
@@ -2,8 +2,15 @@
 
 |Contenido|
 |---------|
+|[Datos generales del Reto](#datos-generales-del-reto)|
 |[Lectura del Archivo](#lectura-del-archivo)|
 |[Analisis de Texto](#analisis-de-un-texto)|
+|[Reemplazar palabra](#reemplazar-palabra)|
+|[Guardar](#guardar)|
+
+## Datos generales del Reto
+
+Para este reto fue muyu importante la investigación de nuevas funciones para poder optimizar el código lo más que se pueda. Cosas como `#define` y funciones de bibliotecas ya trabajadas en clase fueron clave. Sin embargo, la investigación y consulta de como se puede optimizar o el código fue crucial para terminarlo en el tiempo dado, ya que este reto si presentaba un mayor requerimiento de nivel y conocimiento previo que cualquiera de los anteriores. Aún así fue divertido implementar todo lo aprendido durante el semestre en un solo reto.
 
 ## Lectura del archivo
 
@@ -53,10 +60,65 @@ El proposito de esta función es que abra ek archivo y lo lea linea por linea. D
 
     Y por último para guardar los cambios se actualiza el puntero y se aumenta el contador del tamaño.
 
-    ## Analisis de un texto
+## Analisis de un texto
 
-    Lo que se busca hacer con esta función es que haga un analisis completo de un texto dado, su cantidad de palabras, espacios, lineas, y vocales. Cabe recalcar que para el desarrollo de esta funcion se usó una función nueva llamada `isspace`, que se encarga de comprobar si un caracter es un espacio en blanco y si es verdadero devuelve algo difente a 0 y si es falso devuelve un 0.  
+Lo que se busca hacer con esta función es que haga un analisis completo de un texto dado, su cantidad de palabras, espacios, lineas, y vocales. Cabe recalcar que para el desarrollo de esta funcion se usó una función nueva llamada `isspace`, que se encarga de comprobar si un caracter es un espacio en blanco y si es verdadero devuelve algo difente a 0 y si es falso devuelve un 0.  
 
             for(int i = 0; texto[i] != '\0'; i++)
 
-- 
+- Este bucle va por todo el texto caracter por caracter hasta que se encuenrtre con `\0`.
+
+            char c = texto[i];
+
+            if(c != '\n') (*caracteres)++;
+            if(c == ' ') (*espacios)++;
+            if(c == '\n') (*lineas)++;
+
+- Este bucle cuenta los caracteres, espacios y lineas, sumando en cada uno de sus contadores.
+
+            if(!isspace(c) && !dentro_palabra){
+                (*palabra)++;
+                dentro_palabra = 1
+            } else if(isspace(c)){
+                dentro_palabra = 0;}
+
+- Lo que hace este bucle es que detecta si estamos en el inicio de una palabra o no, utilizando la nueva función aprendida `isspace`.
+
+            c = tolower(c);
+            if(c == 'a') vocales[0]++;
+            if(c == 'e') vocales[1]++;
+            if(c == 'i') vocales[2]++;
+            if(c == 'o') vocales[3]++;
+            if(c == 'u') vocales[4]++;
+
+- Aquí con ya una función conocida se convierte la letra que se este analizando a miniuscula, y si es vocal, se suma en el arreglo de datos y en su respectiva posición.
+
+
+## Reemplazar palabra
+
+El proposito de esta funcion es es que reemplaza una palabra por otr y devuelve el texto con los camboios hechos.
+
+Lo primero que se declara son las variables. Asi que `repeticion = 0`, es cantidad de veces que aparece la palabra que se quiere reemplzar. Y con `strlen()`, se calcula el largo de ambas palabras, la vieja y la nueva. 
+
+            while((ptr = strstr(ptr, viejo)) != NULL){
+                repeticion++;
+                ptr = ptr + largo_viejo;
+            }
+
+- Para este condicional se uso una función nueva, `strstr`. Esta función se usa para encontrar la primera ocurrencia de una subcadena y devuelve un puntero la inicio de la subcadena encontrada.
+
+Así que este pedazo de código cuenta cuantas veces aparece la palabra vieja usando esa función nueva.
+
+            nuevo_tam = strlen(texto) + veces * (largo_nuevo - largo_viejo) + 1;
+            resultado = malloc(nuevo_tam);
+            destino = resultado;
+
+- Aqui lo que hace es que calcula la diferencia de caracteres o tamaño entre la palabra vieja y la nueva, para así reservar el espacio de memoria.
+
+- Y ya para finalizar lo que hace el resto de la función es verificar si encuentra la palabra vieja, la reemplza por la nueva, sino sigue su copiado normal.
+
+## Guardar
+
+El proposito de esta función es escribir en un archivo la estadisticas del texto.
+
+Lo primero que hace la función es abrir el archivo en modo escritura `w`. Si no lo puede abrir retorna error. Después escribe las estadisticas del archivo una por una y después escribe el texto final ya modificado.
--- Reto/Reto.c
+++ Reto/Reto.c
@@ -73,17 +73,74 @@ void analizartexto(const char *texto, int *caracteres, int *palabras, int *espac
 
 }
 
-char *reemplzarpalabra(){
+char *reemplzarpalabra(const char *texto, const char *viejo, const char *nuevo){
+    int repeticion = 0;
+    int largo_viejo, largo_nuevo;
+    const char *ptr = texto;
+    int nuevo_tam;
+    char *resultado;
+    char *destino;
+
+    largo_viejo = strlen(viejo);
+    largo_nuevo = strlen(nuevo);
+
+    while((ptr = strstr(ptr, viejo)) != NULL){
+        repeticion++;
+        ptr = ptr + largo_viejo;
+    }
+
+    nuevo_tam = strlen(texto) + veces * (largo_nuevo - largo_viejo) + 1;
+    resultado = malloc(nuevo_tam);
+    destino = resultado;
+
+    while(*texto){
+        if(strstr(texto, viejo) == texto){
+            strcpy(destino, nuevo);
+            destino = destino + largo_nuevo;
+            texto = texto + largo_viejo;
+        } else {
+            *destino++ = *texto++;
+        }
+    }
+    *destino = '\0';
+
+    return resultado;
 
 }
 
-void guardar(){
+void guardar(const char *nombre, const char *texto, in caracteres, int palabras, int espacios, int lineas, int vocales[5]){
+    FILE *archivo = fopen (*nombre, "w");
+    if(!archivo){
+        printf("No se pude cargar el archivo");
+        return 1;
+    }
 
+    fprintf(archivo, "Caracteres: %d\n", caracteres);
+    fprintf(archivo, "Palabras: %d\n", palabras);
+    fprintf(archivo, "Espacios: %d\n", espacios);
+    fprintf(archivo, "Lineas: %d\n", lineas);
+    fprintf(archivo,"\nVocales\n");
+    fprintf(archivo, "a: %d\n", vocales[0]);
+    fprintf(archivo, "e: %d\n", vocales[1]);
+    fprintf(archivo, "i: %d\n", vocales[2]);
+    fprintf(archivo, "o: %d\n", vocales[3]);
+    fprintf(archivo, "u: %d\n", vocales[4]);
+    fprintf(archivo, "%s", texto);
+
+    fclose(archivo);
 }
 
 int main(){
     int opcion;
-    char 
+    int caracteres, palabras, espacios, lineas;
+    int vocales[5] = {0};
+    int cargado = 0;
+    int modificado = 0;
+    char archivo_entrada[max_A], archivo_salida[max_A];
+    char *texto = NULL;
+    char *texto_final = NULL;
+    char palabra_vieja[max_P], palabra_nueva[max_P];
+
 
     do{
         printf("1. Cargar archivo\n");
@@ -98,9 +155,84 @@ int main(){
         switch(opcion){
             case 1:
                 printf("Ingrese el nombre del archivo .txt: ");
+                fgets(archivo_entrada, max_A, stdin);
+                archivo_entrada[strcspn(archivo_entrada, "\0")] = '\0'; //strcspn --> Sirve para encontrar la longitud de la subcadena inicial de una cadena de caracteres
+                texto = leerarchivo(archivo_entrada);
+                if(texto){
+                    printf("Cargado exitos\n");
+                    cargado = 1;
+                    if(texto_final) free(texto_final);
+                    texto_final = texto;
+                }
+                break;
+            
+            case 2:
+                if(!cargado){
+                    printf("Tienes que cargar un archivo\n");
+                    break;
+                }
+
+                analizartexto(texto_final, &caracteres, &palabras, &espacios, &lineas, vocales);
+                printf("Palabras: %d\n", palabras);
+                printf("Espacios: %d\n", espacios);
+                printf("Caracteres: %d\n", caracteres);
+                printf("Lineas: %d\n", lineas);
+                printf("\nVocales\n");
+                printf("a: %d\n", vocales[0]);
+                printf("e: %d\n", vocales[1]);
+                printf("i: %d\n", vocales[2]);
+                printf("o: %d\n", vocales[3]);
+                printf("u: %d\n", vocales[4]);
+
+                break;
+
+            case 3:
+                if(!cargado){
+                    printf("Tienes que cargar un archivo\n");
+                    break;
+                }
+
+                printf("Palabra que deseas cambiar: ");
+                fgets(palabra_vieja, max_P, stdin);
+                palabra_vieja[strcspn(palabra_vieja, "\n")] = '\0';
+
+                printf("Palabra por la que la deseas cambiar: ");
+                fgets(palabra_nueva, max_P, stdin);
+                palabra_nueva[strcspn(palabra_nueva, "\n")] = '\0';
+
+                char *nuevo_texto = reemplzarpalabra(texto_final, palabra_vieja, palabra_nueva);
+                if(texto_final != texto) free(texto_final);
+                texto_final = nuevo_texto;
+                modificado = 1;
+                break;
+
+            case 4:
+                if(!cargado){
+                    printf("Tienes que cargar un archivo\n");
+                    break;
+                }
+
+                printf("Nombre archivo de salida: ");
+                fgets(archivo_salida, max_A, stdin);
+                archivo_salida[strcspn(archivo, "\n")] = '\0';
+
+                analizartexto(texto_final, &caracteres, &palabras, &espacios, &lineas, vocales);
+                guardar(archivo_salida, texto_final, caracteres, palabras, espacios, lineas, vocales);
+                printf("Guardado en %s\n", archivo_salida);
+                break;
+
+            case 5;
+                printf("Saliendo del programa\n");
+                break;
+
+            default:
+             printf("Opcion no valida");
 
         }
-    }
+    } while (opcion != 5);
+
+    if(texto && texto != texto_final) free(texto);
+    if(texto_final) free(texto_final);
 
     return 0;
 }
